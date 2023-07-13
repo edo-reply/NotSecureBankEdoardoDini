0fbbdead56023a3fe065ca5f426d959dfdc7ae7a
Edoardo Dini <e.dini@reply.it>
2023-07-13 12:24:30 +0200

    Fix V2_Log Injection

--- NotSecureBank/src/com/notsecurebank/servlet/AccountViewServlet.java
+++ NotSecureBank/src/com/notsecurebank/servlet/AccountViewServlet.java
@@ -1,6 +1,9 @@
 package com.notsecurebank.servlet;
 
 import java.io.IOException;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,7 +15,9 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class AccountViewServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+    private static final String LOG_DATE_FORMAT = "yyyy-MM-dd";
+
+	private static final long serialVersionUID = 1L;
 
     private static final Logger LOG = LogManager.getLogger(AccountViewServlet.class);
 
@@ -47,9 +52,26 @@ public class AccountViewServlet extends HttpServlet {
 
         // show transactions within the specified date range (if any)
         if (request.getRequestURL().toString().endsWith("showTransactions")) {
+        	
+        	// dangerous code
             String startTime = request.getParameter("startDate");
             String endTime = request.getParameter("endDate");
 
+            SimpleDateFormat acceptedFormat = new SimpleDateFormat(LOG_DATE_FORMAT);
+            
+            try {
+				Date startDate = acceptedFormat.parse(startTime);
+			} catch (ParseException e) {
+				startTime = null;
+			}
+            
+            try {
+				Date endDate = acceptedFormat.parse(endTime);
+			} catch (ParseException e) {
+				endTime = acceptedFormat.format(new Date());
+			}
+            
+            // the input has been checked/substituted so now it is ok
             LOG.info("Transactions within '" + startTime + "' and '" + endTime + "'.");
             RequestDispatcher dispatcher = request.getRequestDispatcher("/bank/transaction.jsp?" + ((startTime != null) ? "&startTime=" + startTime : "") + ((endTime != null) ? "&endTime=" + endTime : ""));
             dispatcher.forward(request, response);
